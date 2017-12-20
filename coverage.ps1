08d79d4b4060a47c1630647aeedc034aee924933
Pekka Järvinen <pekka.jarvinen@gmail.com>
2017-12-20 23:45:41 +0200

    Coverage doesn't understand full paths

--- coverage.ps1
+++ coverage.ps1
@@ -8,16 +8,24 @@ Push-Location "unittest"
 
 Write-Host "Dir: $pwd"
 
-Get-ChildItem . -Recurse -Filter "Test.dll" | Where-Object DirectoryName -NotLike "*obj*" | foreach {
-  $dll = $_.Fullname
+Write-Host "Generating file list.."
+$files = Get-ChildItem . -Recurse -File "Test.dll" | Where-Object DirectoryName -NotLike "*obj*" 
+Write-Host "Done."
+
+Pop-Location
+
+Write-Host "Generating '$covfile'.."
+$files | foreach {
+  $dll = $_.Fullname | Resolve-Path -Relative
   Write-Host "DLL: $dll"
   $targetargs = "$dll -verbose -noshadow"
   Write-Host "Arguments: $targetargs"
   Write-Host "Running Opencover.."
-  OpenCover.Console.exe -log:"All" -register:"user" -target:"xunit.console.exe" -targetargs:"$targetargs" -output:"$covfile"
+  
+  OpenCover.Console.exe -log:"All" -register:"user" -target:"xunit.console.exe" -targetargs:"$targetargs" -filterfile:".\codecov_filter.txt" -output:"$covfile"
+
   Write-Host "Opencover finished."
 }
-Pop-Location
 
 Write-Host "Done."
 
