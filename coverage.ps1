5cdd073ab7b0ded86842d9b2e8ddc5f5ce9d3abe
Pekka Järvinen <pekka.jarvinen@gmail.com>
2017-12-21 00:19:43 +0200

    Use Start-Process

--- coverage.ps1
+++ coverage.ps1
@@ -2,7 +2,7 @@
 Write-Host "Generating coverage file.."
 
 Push-Location "$PSScriptRoot"
-$covfile = "$pwd/coverage.xml"
+$covfile = "$pwd\coverage.xml"
 Write-Host "Coverage file: $covfile"
 Push-Location "unittest"
 
@@ -16,15 +16,31 @@ Pop-Location
 
 Write-Host "Generating '$covfile'.."
 $files | foreach {
-  $dll = $_.Fullname | Resolve-Path -Relative
-  Write-Host "DLL: $dll"
-  $targetargs = "$dll -verbose -noshadow"
-  Write-Host "Arguments: $targetargs"
-  Write-Host "Running Opencover.."
-  
-  OpenCover.Console.exe -log:"All" -register:"user" -target:"xunit.console.exe" -targetargs:"$targetargs" -filterfile:".\codecov_filter.txt" -output:"$covfile"
+	$dll = $_.Fullname | Resolve-Path -Relative
+	Write-Host "DLL: $dll"
+	
+	$args = [string]::Format('-log:"All" -register:"user" -target:"xunit.console.exe" -targetargs:"{0} -verbose -noshadow" -filterfile:"codecov_filter.txt" -output:"{1}"', $dll, $covfile)
+	
+	Write-Host "Arguments: $args"
+	Write-Host "Running Opencover.."
 
-  Write-Host "Opencover finished."
+	$stdErrLog = "$pwd/OpenCover-stderr.log"
+	$stdOutLog = "$pwd/OpenCover-stdout.log"
+
+	$process = Start-Process OpenCover.Console -Wait -PassThru -NoNewWindow -ArgumentList "$args" -RedirectStandardOutput $stdOutLog -RedirectStandardError $stdErrLog
+
+	if ($process.ExitCode -ne 0) {
+		Write-Output "ERROR: running OpenCover failed."
+		Get-Content $stdOutLog
+		Get-Content $stdErrLog
+		Exit 1
+	}
+	
+	Write-Host ""
+	Get-Content $stdOutLog
+	Write-Host ""
+  
+	Write-Host "Opencover finished."
 }
 
 Write-Host "Done."
