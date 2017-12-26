3435f19ec899b9ec81be83f333cf4b159a930341
Pekka Järvinen <pekka.jarvinen@gmail.com>
2017-12-26 05:35:30 +0200

    option 77

---
+++ DHCPNet/DHCPv4/Option/Meta/OptionUserClassIdentity.cs
@@ -0,0 +1,37 @@
+﻿namespace DHCPNet.v4.Option
+{
+    using System;
+
+    /// <summary>
+    /// This option is used by a Dynamic Host Configuration Protocol (DHCP)
+    /// client to optionally identify the type or category of user or
+    /// applications it represents.The information contained in this option
+    /// is an opaque field that represents the user class of which the client
+    /// is a member.Based on this class, a DHCP server selects the
+    /// appropriate address pool to assign an address to the client and the
+    /// appropriate configuration parameters.This option should be
+    /// configurable by a user.
+    /// 
+    /// https://tools.ietf.org/html/rfc3004
+    /// </summary>
+    public class OptionUserClassIdentity : Option
+    {
+        public override byte Code
+        {
+            get
+            {
+                return 77;
+            }
+        }
+
+        public override byte[] GetRawBytes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ReadRaw(byte[] raw)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
