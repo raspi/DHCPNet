05ad145c7f24eeaf3903c9d20c4a4960561889e3
Pekka Järvinen <pekka.jarvinen@gmail.com>
2017-12-26 05:35:13 +0200

    option 62

---
+++ DHCPNet/DHCPv4/Option/String/NetWareIPDomainName.cs
@@ -0,0 +1,21 @@
+﻿namespace DHCPNet.v4.Option
+{
+    /// <summary>
+    /// This option code is used to convey the NetWare/IP domain name used by
+    /// the NetWare/IP product.
+    /// 
+    /// The NetWare/IP Domain in the option is an NVT ASCII string whose 
+    /// length is inferred from the option 'len' field.
+    /// 
+    /// https://tools.ietf.org/html/rfc2242
+    /// </summary>
+    public class OptionNetWareIpDomainName : AOptionString {
+        public override byte Code
+        {
+            get
+            {
+                return 62;
+            }
+        }
+    }
+}
