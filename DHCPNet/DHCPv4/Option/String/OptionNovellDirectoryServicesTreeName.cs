499fbbd14a73e6cd68390c6a6411c2a8624e3f87
Pekka Järvinen <pekka.jarvinen@gmail.com>
2017-12-26 05:34:46 +0200

    option 86

---
+++ DHCPNet/DHCPv4/Option/String/OptionNovellDirectoryServicesTreeName.cs
@@ -0,0 +1,22 @@
+﻿namespace DHCPNet.v4.Option
+{
+    /// <summary>
+    /// This option specifies the name of the NDS tree the client will be
+    /// contacting. NDS tree names are 16-bit Unicode strings. For
+    /// transmission in the NDS Tree Name Option, an NDS tree name is
+    /// transformed into octets using UTF-8. The string should NOT be zero
+    /// terminated.
+    /// 
+    /// Maximum length 255 bytes.
+    /// https://tools.ietf.org/html/rfc2241
+    /// </summary>
+    public class OptionNovellDirectoryServicesTreeName : AOptionString {
+        public override byte Code
+        {
+            get
+            {
+                return 86;
+            }
+        }
+    }
+}
