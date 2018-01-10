e31c0389522552c75ebe39685aaa7dbf970201cc
Pekka Järvinen <pekka.jarvinen@gmail.com>
2018-01-10 19:48:16 +0200

    option 88

---
+++ DHCPNet/DHCPv4/Option/String/OptionBroadcastAndMulticastServiceControllerDomainNameList.cs
@@ -0,0 +1,16 @@
+﻿namespace DHCPNet.v4.Option
+{
+    /// <summary>
+    /// https://tools.ietf.org/html/rfc4280
+    /// </summary>
+    public class OptionBroadcastAndMulticastServiceControllerDomainNameList : AOptionString
+    {
+        public override byte Code
+        {
+            get
+            {
+                return 88;
+            }
+        }
+    }
+}
