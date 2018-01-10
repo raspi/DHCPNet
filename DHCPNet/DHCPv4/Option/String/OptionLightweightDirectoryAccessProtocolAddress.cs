78e40208547ddb47ee675b2fdc01f2783f5ee1b8
Pekka Järvinen <pekka.jarvinen@gmail.com>
2018-01-10 19:48:09 +0200

    option 95

---
+++ DHCPNet/DHCPv4/Option/String/OptionLightweightDirectoryAccessProtocolAddress.cs
@@ -0,0 +1,17 @@
+﻿namespace DHCPNet.v4.Option
+{
+    /// <summary>
+    /// Never published in an RFC
+    /// https://tools.ietf.org/html/rfc3679
+    /// </summary>
+    public class OptionLightweightDirectoryAccessProtocolAddress : AOptionString
+    {
+        public override byte Code
+        {
+            get
+            {
+                return 95;
+            }
+        }
+    }
+}
