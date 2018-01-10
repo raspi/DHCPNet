8cb2093b31f61773a281be12fed36f4830933258
Pekka Järvinen <pekka.jarvinen@gmail.com>
2018-01-10 19:49:24 +0200

    option 89

---
+++ DHCPNet/DHCPv4/Option/IPv4/OptionBroadcastAndMulticastServiceControllerIPv4Address.cs
@@ -0,0 +1,24 @@
+﻿namespace DHCPNet.v4.Option
+{
+    /// <summary>
+    /// The Length byte (Len) is followed by a list of IPv4 addresses
+    /// indicating BCMCS controller IPv4 addresses. 
+    /// 
+    /// The BCMCS controllers MUST be listed in order of preference.
+    /// 
+    /// Minimum length is 4 bytes.
+    /// Length MUST be a multiple of 4. 
+    /// 
+    /// https://tools.ietf.org/html/rfc4280
+    /// </summary>
+    public class OptionBroadcastAndMulticastServiceControllerIPv4Address : AOptionIPAddresses
+    {
+        public override byte Code
+        {
+            get
+            {
+                return 89;
+            }
+        }
+    }
+}
