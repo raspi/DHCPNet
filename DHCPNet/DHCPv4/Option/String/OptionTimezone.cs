27b4f857d4262d400d1817442680025a26fe3b61
Pekka Järvinen <pekka.jarvinen@gmail.com>
2018-01-10 19:47:59 +0200

    option 100

---
+++ DHCPNet/DHCPv4/Option/String/OptionTimezone.cs
@@ -0,0 +1,16 @@
+﻿namespace DHCPNet.v4.Option
+{
+    /// <summary>
+    /// IEEE 1003.1 String
+    /// https://tools.ietf.org/html/rfc4833
+    /// </summary>
+    public class OptionTimezone : AOptionString {
+        public override byte Code
+        {
+            get
+            {
+                return 100;
+            }
+        }
+    }
+}
