fe524a33ffcf1564be270fb81bfb3b47354acbbf
Pekka Järvinen <pekka.jarvinen@gmail.com>
2018-01-10 19:49:35 +0200

    option 93

---
+++ DHCPNet/DHCPv4/Option/Integer/OptionClientSystemArchitectureType.cs
@@ -0,0 +1,28 @@
+﻿namespace DHCPNet.v4.Option
+{
+    using System;
+
+    /// <summary>
+    /// https://tools.ietf.org/html/rfc4578
+    /// </summary>
+    public class OptionClientSystemArchitectureType : Option {
+
+        public override byte Code
+        {
+            get
+            {
+                return 93;
+            }
+        }
+
+        public override byte[] GetRawBytes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ReadRaw(byte[] raw)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
