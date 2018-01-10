a27f134a85214bbe87b04aa18670d008892bf19c
Pekka Järvinen <pekka.jarvinen@gmail.com>
2018-01-10 19:47:50 +0200

    option 101

---
+++ DHCPNet/DHCPv4/Option/String/OptionTimezoneDatabase.cs
@@ -0,0 +1,17 @@
+﻿namespace DHCPNet.v4.Option
+{
+    /// <summary>
+    /// Reference to the TZ Database
+    /// https://tools.ietf.org/html/rfc4833
+    /// </summary>
+    public class OptionTimezoneDatabase : AOptionString
+    {
+        public override byte Code
+        {
+            get
+            {
+                return 101;
+            }
+        }
+    }
+}
