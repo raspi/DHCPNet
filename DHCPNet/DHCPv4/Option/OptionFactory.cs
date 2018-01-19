5085c2b54a51eb0a33b817b31bf07f97ef0da72f
Pekka Järvinen <pekka.jarvinen@gmail.com>
2018-01-19 21:17:04 +0200

    option 123

--- DHCPNet/DHCPv4/Option/OptionFactory.cs
+++ DHCPNet/DHCPv4/Option/OptionFactory.cs
@@ -124,6 +124,7 @@ namespace DHCPNet.v4.Option
                 typeof(OptionSessionInitiationProtocolServers), // 120
                 typeof(OptionClasslessStaticRoute), // 121
                 typeof(OptionCableLabsClientConfiguration), // 122
+                typeof(OptionCoordinateBasedLocationConfigurationInformation), // 123
                 typeof(OptionTftpServerAddresses), // 150
                 typeof(OptionMicrosoftClasslessStaticRoute), // 249 see 121
                 typeof(OptionEnd), // 255*
