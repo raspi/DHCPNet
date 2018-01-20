688ee542d77e2e8ac891bd6130f9babada41232f
Pekka Järvinen <pekka.jarvinen@gmail.com>
2018-01-20 05:47:13 +0200

    added rfc1035 domain name string compression helper

---
+++ DHCPNet/DHCPv4/Option/DomainName.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DHCPNet
+{
+    /// <summary>
+    /// https://tools.ietf.org/html/rfc1035#section-4.1.4
+    /// </summary>
+    public static class DomainName
+    {
+        public static List<string> CompressedBytesToString(byte[] raw)
+        {
+            if (raw.Length == 0)
+            {
+                throw new Exception("Zero length");
+            }
+
+            List<string> addr = new List<string>();
+
+            int offset = 0;
+
+            string tmp = String.Empty;
+
+            do
+            {
+                byte len = raw[offset];
+                offset++;
+
+                char[] c = new char[len];
+
+                for (int i = 0; i < len; i++)
+                {
+                    c[i] = (char)raw[offset + i];
+                }
+
+                tmp += new string(c) + ".";
+
+                offset += len;
+
+                if (raw[offset] == 0)
+                {
+                    tmp = tmp.Substring(0, tmp.Length - 1);
+                    addr.Add(tmp);
+                    offset++;
+                    tmp = String.Empty;
+                }
+            }
+            while (offset < raw.Length);
+
+            return addr;
+        }
+
+        public static byte[] StringToCompressedBytes(List<string> list)
+        {
+            List<byte> raw = new List<byte>();
+
+            foreach (string domainstr in list)
+            {
+                foreach (string part in domainstr.Split('.'))
+                {
+                    raw.Add((byte)part.Length);
+
+                    foreach (char c in part)
+                    {
+                        raw.Add((byte)c);
+                    }
+                }
+
+                raw.Add(0);
+
+            }
+
+            return raw.ToArray();
+        }
+    }
+}
---
+++ UnitTest/Helper.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DHCPNet;
+using Xunit;
+using DHCPNet.v4.Option;
+
+namespace UnitTest.Helper
+{
+
+    public class All
+    {
+        [Fact]
+        public void TestStringToCompressedBytes()
+        {
+            List<string> testList = new List<string> { "example.com", "example.net" };
+
+            byte[] expectedResultBytes =
+                {
+                    7, (byte)'e', (byte)'x', (byte)'a', (byte)'m', (byte)'p', (byte)'l', (byte)'e', 3, (byte)'c',
+                    (byte)'o', (byte)'m', 0, 7, (byte)'e', (byte)'x', (byte)'a', (byte)'m', (byte)'p', (byte)'l',
+                    (byte)'e', 3, (byte)'n', (byte)'e', (byte)'t', 0,
+                };
+
+            byte[] b = DomainName.StringToCompressedBytes(testList);
+
+            Assert.Equal(expectedResultBytes.Length, b.Length);
+
+            for (int i = 0; i < expectedResultBytes.Length; i++)
+            {
+                byte expected = expectedResultBytes[i];
+                byte actual = b[i];
+
+                Assert.True(expected == actual, 
+                    String.Format("Expected {0:x2} Actual: {1:x2}", expected, actual)
+                );
+            }
+
+
+            //Encoding.ASCII.GetBytes("test");
+
+
+            //Assert.Equal("foo", list[0]);
+        }
+
+        [Fact]
+        public void TestCompressedBytesToString()
+        {
+            byte[] testBytes =
+                {
+                    7, (byte)'e', (byte)'x', (byte)'a', (byte)'m', (byte)'p', (byte)'l', (byte)'e', 3, (byte)'c',
+                    (byte)'o', (byte)'m', 0, 7, (byte)'e', (byte)'x', (byte)'a', (byte)'m', (byte)'p', (byte)'l',
+                    (byte)'e', 3, (byte)'n', (byte)'e', (byte)'t', 0,
+                };
+
+            List<string> expected = new List<string> { "example.com", "example.net" };
+
+            List<string> actual = DomainName.CompressedBytesToString(testBytes);
+
+            Assert.Equal(expected.Count, actual.Count);
+
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.True(expected[i] == actual[i],
+                    String.Format("Expected '{0}' Actual: '{1}'", expected[i], actual[i])
+                );
+
+            }
+
+        }
+    }
+}
