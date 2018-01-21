d588f47b8a115ddccd36f55242ac42e2caa72dd3
Pekka Järvinen <pekka.jarvinen@gmail.com>
2018-01-21 11:54:02 +0200

    option 145

---
+++ DHCPNet/DHCPv4/Option/Meta/OptionForceRenewNonceProtocolCapabilities.cs
@@ -0,0 +1,43 @@
+﻿namespace DHCPNet.v4.Option
+{
+    /// <summary>
+    /// The FORCERENEW_NONCE_CAPABLE option contains code 145, length n, and
+    /// a sequence of algorithms the client supports:
+    /// 
+    /// Code Len   Algorithms
+    /// +-----+-----+----+----+----+
+    /// | 145 |  n  | A1 | A2 | A3 | ....
+    /// +-----+-----+----+----+----+
+    /// 
+    /// The client would indicate that it supports the functionality by
+    /// inserting the FORCERENEW_NONCE_CAPABLE option in the DHCP Discover
+    /// and Request messages.
+    /// 
+    /// If the server supports Forcerenew nonce authentication and requires 
+    /// Forcerenew nonce authentication, it will insert the FORCERENEW_NONCE_CAPABLE 
+    /// option in the DHCPOFFER.
+    /// 
+    /// https://tools.ietf.org/html/rfc6704
+    /// </summary>
+    public class OptionForceRenewNonceProtocolCapabilities : Option
+    {
+        public override byte Code
+        {
+            get
+            {
+                return 145;
+            }
+        }
+
+        public override void ReadRaw(byte[] raw)
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+        public override byte[] GetRawBytes()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- DHCPNet/DHCPv4/Option/OptionFactory.cs
+++ DHCPNet/DHCPv4/Option/OptionFactory.cs
@@ -146,6 +146,7 @@ namespace DHCPNet.v4.Option
                 typeof(OptionSessionInitiationProtocolUserAgentConfigurationServiceDomains), // 141
                 typeof(OptionAccessNetworkDiscoveryAndSelectionFunctionIPv4Addresses), // 142
                 typeof(OptionCoordinateBasedLocationConfigurationInformationLocation), // 144
+                typeof(OptionForceRenewNonceProtocolCapabilities), // 145
                 typeof(OptionTftpServerAddresses), // 150
                 typeof(OptionMicrosoftClasslessStaticRoute), // 249 see 121
                 typeof(OptionEnd), // 255*
