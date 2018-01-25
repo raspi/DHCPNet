d90c285ddacc5ca7adc0ddebe54d4f6c7685938a
Pekka Järvinen <pekka.jarvinen@gmail.com>
2018-01-26 01:43:27 +0200

    check

--- DHCPNet/DHCPv4/Option/Meta/OptionBulkLeaseQueryStatusCode.cs
+++ DHCPNet/DHCPv4/Option/Meta/OptionBulkLeaseQueryStatusCode.cs
@@ -74,6 +74,11 @@ namespace DHCPNet.v4.Option
         /// <inheritdoc />
         public override void ReadRaw(byte[] raw)
         {
+            if (raw.Length == 0)
+            {
+                throw new OptionLengthZeroException();
+            }
+
             throw new System.NotImplementedException();
         }
 
