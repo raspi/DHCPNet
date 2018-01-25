2b9d3f7b24fe1c2c3ac29de63996142597da863c
Pekka Järvinen <pekka.jarvinen@gmail.com>
2018-01-26 01:45:03 +0200

    check

--- DHCPNet/DHCPv4/Option/Time/AOptionTimeUint32.cs
+++ DHCPNet/DHCPv4/Option/Time/AOptionTimeUint32.cs
@@ -16,6 +16,16 @@ namespace DHCPNet.v4.Option
         /// <inheritdoc />
         public override void ReadRaw(byte[] raw)
         {
+            if (raw.Length == 0)
+            {
+                throw new OptionLengthZeroException();
+            }
+
+            if (raw.Length != 4)
+            {
+                throw new OptionLengthNotExactException("Length is not 4.");
+            }
+
             Time = new TimeSpan(0, 0, (int)BitConverter.ToUInt32(raw, 0));
         }
     }
