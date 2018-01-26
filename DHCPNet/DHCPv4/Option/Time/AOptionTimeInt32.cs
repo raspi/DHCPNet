cb4884a033368766ad4f19ae82e5b9dca538e7a9
Pekka Järvinen <pekka.jarvinen@gmail.com>
2018-01-26 15:22:01 +0200

    check

--- DHCPNet/DHCPv4/Option/Time/AOptionTimeInt32.cs
+++ DHCPNet/DHCPv4/Option/Time/AOptionTimeInt32.cs
@@ -3,7 +3,7 @@
 namespace DHCPNet.v4.Option
 {
     /// <summary>
-    /// Option for 32 bit int seconds
+    /// Option for 32 bit int seconds, 4 bytes
     /// </summary>
     public abstract class AOptionTimeInt32 : AOptionTimeBase
     {
@@ -21,6 +21,10 @@ namespace DHCPNet.v4.Option
                 throw new OptionLengthZeroException();
             }
 
+            if (raw.Length != 4)
+            {
+                throw new OptionLengthNotExactException("Length is not 4.");
+            }
 
             Time = new TimeSpan(0, 0, BitConverter.ToInt32(raw, 0));
         }
