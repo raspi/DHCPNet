769c81f12c03bb410462a0874852ef0045767a3b
Pekka Järvinen <pekka.jarvinen@gmail.com>
2018-01-26 22:44:07 +0200

    doc

--- DHCPNet/DHCPv4/Option/Meta/EMessageType.cs
+++ DHCPNet/DHCPv4/Option/Meta/EMessageType.cs
@@ -2,46 +2,55 @@ namespace DHCPNet.v4.Option
 {
     /// <summary>
     /// DHCP Message Type
+    /// https://tools.ietf.org/html/rfc2132#section-9.6
     /// </summary>
     public enum EMessageType : byte
     {
         /// <summary>
         /// Client Broadcast: Can I have an IP?
+        /// https://tools.ietf.org/html/rfc2132#section-9.6
         /// </summary>
         Discover = 1,
 
         /// <summary>
         /// Server Broadcast: Here's an IP
+        /// https://tools.ietf.org/html/rfc2132#section-9.6
         /// </summary>
         Offer,
 
         /// <summary>
         /// Client Broadcast: I'll take that IP (Also start for renewals)
+        /// https://tools.ietf.org/html/rfc2132#section-9.6
         /// </summary>
         Request,
 
         /// <summary>
         /// Client Broadcast: Sorry I can't use that IP
+        /// https://tools.ietf.org/html/rfc2132#section-9.6
         /// </summary>
         Decline,
 
         /// <summary>
         /// Server: Yes you can have that IP
+        /// https://tools.ietf.org/html/rfc2132#section-9.6
         /// </summary>
         Acknowledge,
 
         /// <summary>
         /// Server: No you cannot have that IP
+        /// https://tools.ietf.org/html/rfc2132#section-9.6
         /// </summary>
         NotAcknowledged,
 
         /// <summary>
         /// Client: I don't need that IP anymore
+        /// https://tools.ietf.org/html/rfc2132#section-9.6
         /// </summary>
         Release,
 
         /// <summary>
         /// Client: I have this IP and there's nothing you can do about it
+        /// https://tools.ietf.org/html/rfc2132#section-9.6
         /// </summary>
         Inform,
 
