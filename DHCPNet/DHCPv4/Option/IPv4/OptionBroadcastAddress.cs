f5ecf5e0b0f2c08642e3de92d39dbc43291d0dbb
Pekka Järvinen <pekka.jarvinen@gmail.com>
2018-01-26 15:50:12 +0200

    doc

--- DHCPNet/DHCPv4/Option/IPv4/OptionBroadcastAddress.cs
+++ DHCPNet/DHCPv4/Option/IPv4/OptionBroadcastAddress.cs
@@ -3,12 +3,15 @@ using System;
 namespace DHCPNet.v4.Option
 {
     /// <summary>
+    /// Broadcast Address
+    /// 
     /// This option specifies the broadcast address in use 
     /// on the client's subnet. 
     /// Legal values for broadcast addresses are 
     /// specified in section 3.2.1.3
     /// 
     /// Length 4 bytes
+    /// https://tools.ietf.org/html/rfc2132#section-5.3
     /// </summary>
     public class OptionBroadcastAddress : AOptionIPAddress
     {
