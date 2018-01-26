ba28bfb790b6e5aa22c9982c80fc5190ccb17f12
Pekka Järvinen <pekka.jarvinen@gmail.com>
2018-01-26 16:58:22 +0200

    doc

--- DHCPNet/DHCPv4/Option/String/OptionBootFileName.cs
+++ DHCPNet/DHCPv4/Option/String/OptionBootFileName.cs
@@ -3,10 +3,14 @@ using System;
 namespace DHCPNet.v4.Option
 {
     /// <summary>
+    /// Bootfile name
+    /// 
     /// This option is used to identify a bootfile when the 
     /// 'file' field in the DHCP header has been used for DHCP options.
     /// 
     /// Minimum length is 1.
+    /// 
+    /// https://tools.ietf.org/html/rfc2132#section-9.5
     /// </summary>
     public class OptionBootFileName : AOptionString
     {
